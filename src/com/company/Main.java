82bf0ffa74d06570f27ac6ffdd4c98af48be4060
nathan-jayanthan <nathan.jayanthan@yahoo.com>
2020-07-04 14:17:31 -0700

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    //this is a "do" while loop which completes one action first that goes to the while
+
+    public static void main(String[] args) {
+            int counter = 0;
+            do{
+                System.out.println(counter);
+                counter++;
+
+            }
+            while(counter <= 10);
+
+    }
+}
